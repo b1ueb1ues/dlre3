235234b20165b240feebe9f53c09320d4c8a16a1
b1ueb1ues <bblues@live.cn>
2020-08-04 18:33:42 +0800

    save the day

--- frida/mod/chainnum.js
+++ frida/mod/chainnum.js
@@ -4,13 +4,20 @@ gl.invincible();
 gl.dummy();
 
 hook(
-offset.chainctrl.add;
+offset.chainctrl.add
 ,{ // chain add
     onEnter: function(args){
         //console.log(args[2]);
         var tis = ptr(args[0]);
-        var cn = tis.add(0x10).readInt();
-        console.log(cn);
+        //var timer = tis.add(0x10).readFloat();
+        //console.log(timer);
+        //var vt = tis.add(0x14).readFloat();
+        //console.log(vt);
+
+        var cnd = tis.add(0x18).readPointer();
+        console.log(hexdump(arrow(cnd, 0)));
+        console.log(arrow.i(cnd, 0));
+
     },
     onLeave: function(ret){
     }
--- frida/skada.js
+++ frida/skada.js
@@ -2,40 +2,8 @@
 //gl.dummy();
 //gl.attack(10000);
 //gl.invincible();
+savetheday();
 
-// save the day
-hook(
-offset.ingameuictrl.showdamageui
-,{
-    onEnter: function (args) {
-        //this.context.x3 = 0; //crit
-        //this.context.x5 = 2;  //ele
-        this.context.sp.writeInt(0); // isplayerctrl
-    }
-});
-
-// save the day
-hook(
-offset.ingameuictrl.setmovein
-,{
-    onEnter: function (args) {
-        if (args[1] == 0)
-            this.context.x1 = 1;
-    }
-});
-
-// save the day
-hook(offset.maingameleavealonechecker.setleavealonetime, {
-    onEnter: function(args){
-        this.tis = args[0];
-        send('unsetleavealone',tstderr);
-    },
-    onLeave: function(retval){
-        var tis = this.tis;
-        tis.add(offset.maingameleavealonechecker.warnningtime).writeFloat(100000);
-        tis.add(offset.maingameleavealonechecker.exittime).writeFloat(100000);
-    }
-});
 
 // time init
 var g_gpt = new NativeFunction(lib_base.add(
